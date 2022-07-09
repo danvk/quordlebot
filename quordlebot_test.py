a702e4191a5b9cfee83115cd64d7a095812185be
Dan Vanderkam <danvdk@gmail.com>
2022-07-09 14:40:36 -0400

    tests for double letters

--- quordlebot_test.py
+++ quordlebot_test.py
@@ -7,3 +7,10 @@ def test_result_for_guess_simple():
     assert result_for_guess('HAVEN', 'MAVEN') == '.gggg'
     assert result_for_guess('MAVEN', 'MAVEN') == 'ggggg'
     assert result_for_guess('STERN', 'CLAMP') == '.....'
+
+
+def test_result_double_letters():
+    assert result_for_guess('APPLE', 'APPLE') == 'ggggg'
+    assert result_for_guess('APPLE', 'PLUMP') == 'yy..y'
+    assert result_for_guess('APPLE', 'POPES') == 'y.gy.'
+    assert result_for_guess('ROPES', 'POPES') == '.gggg'
